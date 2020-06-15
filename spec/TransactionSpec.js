966a1a8cc3c2a675fdcbedc869f5551903c0e892
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-06-15 15:10:00 +0100

    Abstract away makeWithdrawal method to Transaction class

--- spec/TransactionSpec.js
+++ spec/TransactionSpec.js
@@ -16,4 +16,11 @@ describe('Transaction', function() {
       expect(trans.makeDeposit(1000, 0)).toEqual({date: "15/06/2020", credit: "1000.00", debit: "", balance: "1000.00"})
     })
   })
+  describe('#makeWithdrawal', function() {
+    it('returns an object with transaction details', function() {
+      let trans = new Transaction();
+      spyOn(Date, 'now').and.returnValue(1592226530185);
+      expect(trans.makeWithdrawal(500, 1000)).toEqual({date: "15/06/2020", credit: "", debit: "500.00", balance: "500.00"})
+    })
+  })
 })
--- src/Transaction.js
+++ src/Transaction.js
@@ -15,6 +15,16 @@ class Transaction {
     }
     return trans;
   }
+  makeWithdrawal(amount, balance) {
+    balance -= amount;
+    let trans = {
+      date: this._formatDate(),
+      credit: "",
+      debit: this._formatAmount(amount),
+      balance: this._formatAmount(balance)
+    }
+    return trans;
+  }
   _formatDate() {
     return new Date(Date.now()).toLocaleDateString();
   }
